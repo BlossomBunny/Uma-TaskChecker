0cca6b8dd26a81009d2ffa93d36f56813316a8dd
BlossomBunny <admin@hanafubukidoll.co.uk>
2026-05-04 15:17:37 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,8 +4,11 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll([
-        './',
-        'index.html'
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icon-192.png',
+        '/icon-512.png'
       ]);
     })
   );

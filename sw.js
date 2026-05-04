828b3ce8852ff7a4f6d2b9bb61144a9fe16540d0
BlossomBunny <admin@hanafubukidoll.co.uk>
2026-05-04 15:30:48 +0100

    Refactor service worker for cache management

--- sw.js
+++ sw.js
@@ -1,23 +1,36 @@
 const CACHE_NAME = 'tachyon-v1.4.2';
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/icon-192.png',
-        '/icon-512.png'
-      ]);
-    })
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/manifest.json',
+                '/icon-192.png',
+                '/icon-512.png'
+            ]);
+        })
+    );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            // Return cached version or fetch from network
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// Clean up old caches
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            );
+        })
+    );
 });
